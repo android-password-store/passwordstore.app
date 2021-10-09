46bd96302cafccff03019b3476299ee58c0932df
Devansh Bajaj <Devansh21db.db@gmail.com>
2021-10-09 21:00:17 +0530

    feat: add navbar and description

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
-  <img
-    alt="Vue logo"
-    style="width: 200px; height: 200px;"
-    src="@/assets/FG.png"
-  />
+  <Navbar />
   <HelloWorld msg="Welcome to Android Password Store" />
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import Navbar from "@/components/Navbar.vue";
 export default {
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar
   }
 };
 </script>
 
-<style lang="scss">
+<style>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin: 2rem;
+  margin: 0 2rem;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="hello">
+    <img
+    alt="Vue logo"
+    style="width: 200px; height: 200px;"
+    src="@/assets/FG.png"
+  />
     <h1>{{ msg }}</h1>
-    <a href="https://play.google.com/store/apps/details?id=dev.msfjarvis.aps">
-      <img src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" 
-          alt="Get it on Google Play"
-          height="80" />
-    </a>
-    <a href="https://f-droid.org/packages/dev.msfjarvis.aps">
-      <img src="https://fdroid.gitlab.io/artwork/badge/get-it-on.png"
-          alt="Get it on F-Droid"
-          height="80" />
-    </a>
+    <p>
+      Android client for pass. Password Store aims to be a feature-complete client and implements most of its capabilities.
+    </p>
   </div>
 </template>
 
@@ -25,7 +23,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.hello {
+  margin: 2rem 0;
 }
 </style>
