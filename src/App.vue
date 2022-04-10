bba7846c2ae852f78fcce0df45a0a02376edfecc
Devansh Bajaj <devansh21db.db@gmail.com>
2022-04-10 16:04:27 +0530

    feat: add docs button, fetch stars & backers (#5)
    from API

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div class="section1">
-    <Navbar />
-    <HelloWorld msg="Android Password Store" />
-  </div>
+	<div class="section1">
+		<Navbar />
+		<Home msg="Android Password Store" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
+import Home from "@/components/Home.vue";
 import Navbar from "@/components/Navbar.vue";
 export default {
-  components: {
-    HelloWorld,
-    Navbar
-  }
+	components: {
+		Home,
+		Navbar,
+	},
 };
 </script>
 
 <style>
-
 #app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 0 2rem;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin: 0 2rem;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,72 +1,87 @@
 <template>
-  <nav class="navContainer">
-    <div class="wrapper">
-      <img class="logo" src="@/assets/FG.png" >
-    </div>
-    <div class="btnContainer">
-      <a class="btn" href="https://play.google.com/store/apps/details?id=dev.msfjarvis.aps" target="_blank" rel="noreferrer">
-        <img class="logoPlay" src="@/assets/play.svg">
-      </a>
-      <a class="btn" href="https://f-droid.org/packages/dev.msfjarvis.aps" target="_blank" rel="noreferrer">
-        <img class="logoPlay" src="@/assets/fdroid.svg">
-      </a>
-      <a class="btn" href="https://github.com/android-password-store" target="_blank" rel="noreferrer">
-        <img class="logoPlay" src="@/assets/github.svg">
-      </a>
-    </div>
-  </nav>
+	<nav class="navContainer">
+		<div class="wrapper">
+			<img class="logo" src="@/assets/FG.png" />
+		</div>
+		<div class="btnContainer">
+			<a
+				class="btn"
+				href="https://play.google.com/store/apps/details?id=dev.msfjarvis.aps"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<img class="logoPlay" src="@/assets/play.svg" />
+			</a>
+			<a
+				class="btn"
+				href="https://f-droid.org/packages/dev.msfjarvis.aps"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<img class="logoPlay" src="@/assets/fdroid.svg" />
+			</a>
+			<a
+				class="btn"
+				href="https://github.com/android-password-store"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<img class="logoPlay" src="@/assets/github.svg" />
+			</a>
+		</div>
+	</nav>
 </template>
 
 <style scoped>
-  .navContainer {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between ;
-    margin-top: 1rem; 
-  }
+.navContainer {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
+	margin-top: 1rem;
+}
 
-  .wrapper {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
+.wrapper {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+}
 
-  .logo {
-    height: 82px;
-    width: 82px;
-  }
+.logo {
+	height: 82px;
+	width: 82px;
+}
 
-  .btnContainer {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 2rem;
-  }
+.btnContainer {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 2rem;
+}
 
-  .btn :hover {
-    transition: box-shadow 0.5s ease-in;
-     box-shadow: 1px 17px 41px 1px rgba(112,144,176,0.29);
-    -webkit-box-shadow: 1px 17px 41px 1px rgba(112,144,176,0.29);
-    -moz-box-shadow: 1px 17px 41px 1px rgba(112,144,176,0.29);
-  }
+.btn :hover {
+	transition: box-shadow 0.5s ease-in;
+	box-shadow: 1px 17px 41px 1px rgba(112, 144, 176, 0.29);
+	-webkit-box-shadow: 1px 17px 41px 1px rgba(112, 144, 176, 0.29);
+	-moz-box-shadow: 1px 17px 41px 1px rgba(112, 144, 176, 0.29);
+}
 
-  .logoPlay {
-    border: 1px solid #E9EDF7;
-    border-radius: 6px;
-    padding: 0.4rem;
-    height: 40px;
-    width: 40px;
-  }
+.logoPlay {
+	border: 1px solid #e9edf7;
+	border-radius: 6px;
+	padding: 0.4rem;
+	height: 40px;
+	width: 40px;
+}
 
-  @media (max-width: 760px) {
-     .logo {
-    height: 54px;
-    width: 54px;
-  }
-    .logoPlay {
-    height: 32px;
-    width: 32px;
-  }
-  }
+@media (max-width: 760px) {
+	.logo {
+		height: 54px;
+		width: 54px;
+	}
+	.logoPlay {
+		height: 32px;
+		width: 32px;
+	}
+}
 </style>
