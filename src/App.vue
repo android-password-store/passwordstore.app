7e33e4f1e6596fd79a271d5170f386740ab0936c
Harsh Shandilya <me@msfjarvis.dev>
2022-05-02 10:40:36 +0530

    refactor: format with eslint

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-	<div class="section1">
-		<Navbar />
-		<Home msg="Android Password Store" />
-	</div>
+  <div class="section1">
+    <Navbar />
+    <Home msg="Android Password Store" />
+  </div>
 </template>
 
 <script>
 import Home from "@/components/Home.vue";
 import Navbar from "@/components/Navbar.vue";
 export default {
-	components: {
-		Home,
-		Navbar,
-	},
+  components: {
+    Home,
+    Navbar,
+  },
 };
 </script>
 
 <style>
 #app {
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin: 0 2rem;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0 2rem;
 }
 </style>
